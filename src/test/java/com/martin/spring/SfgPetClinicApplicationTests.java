01ba2e308a0158eb7f231fc1187d6e510686bf3f
Joe Martin <martin@360works.com>
2022-07-07 23:48:07 -0400

    Initial Commit

---
+++ src/test/java/com/martin/spring/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
