f6430c6d26e4b87aeffadf03eb6335e4c3ed000d
Joe Martin <martin@360works.com>
2022-07-10 20:26:57 -0400

    Added BaseEntity and extended proper classes to use it. Refactored most shared operation in Service to the CrudService interface. Closes #10

--- pet-clinic-data/src/main/java/com/joevmartin/spring/services/PetService.java
+++ pet-clinic-data/src/main/java/com/joevmartin/spring/services/PetService.java
@@ -5,11 +5,5 @@ import com.joevmartin.spring.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-	Pet findById( Long id);
-
-	Pet save(Pet owner);
-
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
