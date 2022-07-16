84de5553623ee739c786fba8d70f217f977b9ff8
Joe Martin <martin@360works.com>
2022-07-16 13:34:49 -0400

    listing all owners on owners page, closes #22

--- pet-clinic-web/src/main/java/com/joevmartin/spring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/joevmartin/spring/controllers/OwnerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
 	private final OwnerServiceMap ownerServiceMap;
@@ -14,7 +15,7 @@ public class OwnerController {
 		this.ownerServiceMap = ownerServiceMap;
 	}
 
-	@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String listOwners( Model model ) {
 
 		model.addAttribute( "owners", ownerServiceMap.findAll() );
