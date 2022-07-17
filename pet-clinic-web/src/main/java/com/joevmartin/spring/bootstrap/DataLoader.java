8096a5a08d095f0634f8c3f8240bdb788f14aa55
Joe Martin <martin@360works.com>
2022-07-17 00:23:25 -0400

    Added pet types in bootstrap, closes #31

--- pet-clinic-web/src/main/java/com/joevmartin/spring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/joevmartin/spring/bootstrap/DataLoader.java
@@ -6,6 +6,7 @@ import com.joevmartin.spring.model.PetType;
 import com.joevmartin.spring.model.Vet;
 import com.joevmartin.spring.services.OwnerService;
 import com.joevmartin.spring.services.PetService;
+import com.joevmartin.spring.services.PetTypeService;
 import com.joevmartin.spring.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,59 +19,92 @@ public class DataLoader  implements CommandLineRunner {
 	private final OwnerService ownerService;
 	private final VetService vetService;
 	private final PetService petService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader( OwnerService ownerService, VetService vetService, PetService petService ) {
+	public DataLoader( OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService ) {
 
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petService = petService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run( String... args ) throws Exception {
 
-		final Owner joe = new Owner();
+		final PetType ferretType;
 		{
-			joe.setFirstName( "Joe" );
-			joe.setLastName( "Martin" );
-			ownerService.save( joe );
+			PetType temp = new PetType();
+			temp.setName( "Ferret" );
+			ferretType = petTypeService.save( temp );
 		}
 
-		final Owner lb = new Owner();
+
+		final PetType dogType;
 		{
-			lb.setFirstName( "Lora-Beth" );
-			lb.setLastName( "Allen" );
-			ownerService.save( lb );
+			PetType temp = new PetType();
+			temp.setName( "Dog" );
+			dogType = petTypeService.save( temp );
+		}
+
+
+		final Owner joe;
+		{
+			Owner temp = new Owner();
+			temp.setFirstName( "Joe" );
+			temp.setLastName( "Martin" );
+			joe = ownerService.save( temp );
+		}
+
+		final Owner lb;
+		{
+			Owner temp = new Owner();
+			temp.setFirstName( "Lora-Beth" );
+			temp.setLastName( "Allen" );
+			lb = ownerService.save( temp );
 		}
 
 		System.out.println("Loaded Owners...");
 
+		final Vet EllieVet;
 		{
 			final Vet vet = new Vet();
 			vet.setFirstName( "Ellie" );
 			vet.setLastName( "Belly" );
-			vetService.save( vet );
+			EllieVet = vetService.save( vet );
 		}
+
+		final Vet FennyVet;
 		{
 			final Vet vet = new Vet();
 			vet.setFirstName( "Fenny" );
 			vet.setLastName( "Bo-Benny" );
-			vetService.save( vet );
+			FennyVet = vetService.save( vet );
 		}
 
 		System.out.println("Vets loaded...");
 
+		final Pet petFerret;
 		{
 			final Pet pet = new Pet();
 			pet.setOwner( joe );
 			pet.setBirthdate( LocalDate.of(2020, 1, 1) );
-			final PetType petType = new PetType();
-			petType.setName( "Ferret" );
-			pet.setPetType( petType );
-
-			petService.save( pet );
-
+			pet.setPetType( ferretType );
+			petFerret = petService.save( pet );
 		}
+		joe.getPets().add( petFerret );
+		ownerService.save( joe );
+
+		final Pet petDog;
+		{
+			final Pet pet = new Pet();
+			pet.setOwner( lb );
+			pet.setBirthdate( LocalDate.of(2020, 1, 1) );
+			pet.setPetType( dogType );
+			petDog = petService.save( pet );
+		}
+		lb.getPets().add( petDog );
+		ownerService.save( lb );
 
 	}
 
