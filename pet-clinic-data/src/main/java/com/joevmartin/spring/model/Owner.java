16137d907ef01107353e11a9266df9349bf9f9c1
Joe Martin <martin@360works.com>
2022-07-16 23:41:32 -0400

    Futher applied the layout to the owners page, fleshing it out some more

--- pet-clinic-data/src/main/java/com/joevmartin/spring/model/Owner.java
+++ pet-clinic-data/src/main/java/com/joevmartin/spring/model/Owner.java
@@ -1,4 +1,16 @@
 package com.joevmartin.spring.model;
 
+import java.util.List;
+
 public class Owner extends Person {
+
+	private List<Pet> pets;
+
+	public List<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets( List<Pet> pets ) {
+		this.pets = pets;
+	}
 }
