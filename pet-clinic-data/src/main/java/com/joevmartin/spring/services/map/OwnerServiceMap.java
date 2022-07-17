3bea7219c48fac08f9fc61ae9f21bd7611d951f2
Joe Martin <martin@360works.com>
2022-07-17 00:46:05 -0400

    Enhanced owners in data loader with pets and petypes.  Closes #32

--- pet-clinic-data/src/main/java/com/joevmartin/spring/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/joevmartin/spring/services/map/OwnerServiceMap.java
@@ -1,9 +1,44 @@
 package com.joevmartin.spring.services.map;
 
 import com.joevmartin.spring.model.Owner;
+import com.joevmartin.spring.model.Pet;
 import com.joevmartin.spring.services.OwnerService;
+import com.joevmartin.spring.services.PetService;
+import com.joevmartin.spring.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerServiceMap extends PersonServiceMap<Owner> implements OwnerService {
+
+	private PetTypeService petTypeService;
+	private PetService petService;
+
+	public OwnerServiceMap( PetTypeService petTypeService, PetService petService ) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
+
+	@Override
+	public Owner save( Owner model ) {
+
+		if ( model != null ) {
+			if ( model.getPets() != null ) {
+
+				model.getPets().forEach( pet -> {
+					if ( pet.getPetType() != null) {
+						if( pet.getPetType().getId() == null) {
+							pet.setPetType( petTypeService.save( pet.getPetType() ) );
+						}
+					}
+
+					if (pet.getId() == null) {
+						final Pet savedPet = petService.save( pet );
+						pet.setId( savedPet.getId() );
+					}
+				});
+			}
+		}
+
+		return super.save( model );
+	}
 }
